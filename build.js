3778a7117d629511745b1647df23ba179497f97a
Morgan Stone <mstone@designbymorgan.com>
2017-02-05 16:11:49 -0600

    contentful example

---
+++ build.js
@@ -0,0 +1,39 @@
+const Metalsmith = require('metalsmith')
+
+const layouts = require('metalsmith-layouts')
+const assets = require('metalsmith-assets')
+const sass = require('metalsmith-sass')
+const markdown = require('metalsmith-markdown')
+const dataMarkdown = require('metalsmith-data-markdown')
+const contentful = require('contentful-metalsmith')
+
+const handlebars = require('handlebars')
+
+
+Metalsmith(__dirname)
+  .source('src/pages')
+  .destination('dist')
+  .use(contentful({
+    space_id: 'sx4b7m2fosu5',
+    access_token: '65a7a0532ea85cafc91b69f0a79b28dd31af5cabfe11a4f93ea1284a8e5760c7'
+  }))
+  .use(layouts({
+    engine: 'handlebars',
+    partials: 'src/partials',
+    directory: 'src/layouts'
+  }))
+  .use(assets({
+    source: 'src/assets/',
+    destination: 'assets/'
+  }))
+  .use(sass({
+    outputStyle: 'compressed'
+  }))
+  .use(markdown())
+  .use(dataMarkdown({
+    removeAttributeAfterwards: true
+  }))
+  .build((err) => {
+    if (err) throw err
+    console.log('Successfully build metalsmith')
+  })
